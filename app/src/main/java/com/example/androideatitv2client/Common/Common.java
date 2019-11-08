8907755b6cc5a6602f207d0673014ee90f022373
ChaseKnowlden <knowlden887@gmail.com>
2019-11-08 16:22:25 -0500

    Initial commit

---
+++ app/src/main/java/com/example/androideatitv2client/Common/Common.java
@@ -0,0 +1,8 @@
+package com.example.androideatitv2client.Common;
+
+import com.example.androideatitv2client.Model.UserModel;
+
+public class Common {
+    public static final String USER_REFERENCES = "Users";
+    public static UserModel currentUser;
+}
---
+++ app/src/main/java/com/example/androideatitv2client/Remote/RetrofitICloudClient.java
@@ -0,0 +1,18 @@
+package com.example.androideatitv2client.Remote;
+
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetrofitICloudClient {
+    private static Retrofit instance;
+    public static Retrofit getInstance() {
+        if (instance == null)
+            instance = new Retrofit.Builder()
+                    .baseUrl("https://us-central1-comicreader-a3f36.cloudfunctions.net/")
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                    .build();
+        return instance;
+    }
+}
